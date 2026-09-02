617ed39364c8886a5a810536e8bb04a679d267d8
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-09-02 17:30:37 +0600

    a

---
+++ products-service/src/main/java/com/appsdeveloperblog/products/service/handler/ProductCommandsHandler.java
@@ -0,0 +1,45 @@
+package com.appsdeveloperblog.products.service.handler;
+
+import com.appsdeveloperblog.core.dto.Product;
+import com.appsdeveloperblog.core.dto.commands.ReserveProductCommand;
+import com.appsdeveloperblog.core.dto.events.ProductReservedEvent;
+import com.appsdeveloperblog.products.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics="${products.commands.topic.name}")
+public class ProductCommandsHandler {
+
+    private final ProductService productService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+//    private final KafkaTemplate<String, Object> kafkaTemplate;
+//    private final String productEventsTopicName;
+
+    public ProductCommandsHandler(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @KafkaHandler
+    public void handleCommand(@Payload ReserveProductCommand command){
+
+        try {
+            Product desiredProduct = new Product(command.getProductId(), command.getProductQuantity());
+            Product reservedProduct = productService.reserve(desiredProduct, command.getOrderId());
+            ProductReservedEvent productReservedEvent = new ProductReservedEvent(command.getOrderId(),
+                    command.getProductId(),
+                    reservedProduct.getPrice(),
+                    command.getProductQuantity());
+
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
+
+    }
+
+}
