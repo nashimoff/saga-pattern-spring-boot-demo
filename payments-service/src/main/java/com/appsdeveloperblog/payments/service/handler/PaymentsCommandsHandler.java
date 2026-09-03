2df00993b30c88a77b7cbb2bf30f1018575f3c5c
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-09-03 18:10:51 +0600

    a

---
+++ payments-service/src/main/java/com/appsdeveloperblog/payments/service/handler/PaymentsCommandsHandler.java
@@ -0,0 +1,38 @@
+package com.appsdeveloperblog.payments.service.handler;
+
+import com.appsdeveloperblog.core.dto.Payment;
+import com.appsdeveloperblog.core.dto.commands.ProcessPaymentCommand;
+import com.appsdeveloperblog.core.exceptions.CreditCardProcessorUnavailableException;
+import com.appsdeveloperblog.payments.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics="${payments.commands.topic.name}")
+public class PaymentsCommandsHandler {
+
+    private final PaymentService paymentService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public PaymentsCommandsHandler(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @KafkaHandler
+    public void handleCommand(@Payload ProcessPaymentCommand command){
+
+        try {
+            Payment payment = new Payment(command.getOrderId(),
+                    command.getProductId(),
+                    command.getProductPrice(),
+                    command.getProductQuantity());
+            Payment processedPayment = paymentService.process(payment);
+        } catch (CreditCardProcessorUnavailableException e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
+    }
+}
